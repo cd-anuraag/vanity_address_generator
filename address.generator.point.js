6e52fcc28f66d5559b3f6d2bdc394ad8517a7fed
anuraag <anuraag.bansal@clickdee.com>
2023-08-20 19:13:54 +0530

    changes

--- address.generator.point.js
+++ address.generator.point.js
@@ -1,36 +1,60 @@
-const elliptic = require('elliptic');
+const EC = require('elliptic');
 const crypto = require('crypto');
-const BigNumber = require('bn.js');
-const jsSha3 = require('js-sha3');
-const keccak256 = require('keccak256');
-const targetPrefix = 'a';
-
-const ec = new elliptic.ec('secp256k1');
+const BN = require('bn.js');
+const sha3 = require('js-sha3');
+const targetPrefix = 'ab';
 
 function generatePrivateKey() {
     return crypto.randomBytes(32).toString('hex');
 }
 
-(async () => {
-    let privateKey, address;
-    privateKey = "f8f8a2f43c8376ccb0871305060d7b27b0554d2cc72bccf41b2705608452f315";
-    const G = ec.g;
+function privateKeyToAddress(privateKey) {
+    try {
+        const ec = new EC.ec('secp256k1');
 
-    const pp = G.mul(privateKey);
-    const x = pp.getX().toString('hex')
-    const y = pp.getY().toString('hex');
-    const concat = (x + y);
-    const hash = keccak256(concat).toString('hex');
-    console.log(hash)
-    // const hash = keccak256("").toString('hex');
-    // console.log(hash)
-    //
-    //     if (address.startsWith(targetPrefix)) {
-    //         break;
-    //     }
-    //     console.log("finding")
-    // }
-    // console.log('Private key: ', privateKey);
-    // console.log('Address: ', address);
+        const SK = new BN(privateKey, 16);
+
+        const G = ec.g;
+        const pp = G.mul(SK);
+
+        const x = pp.getX().toBuffer();
+
+        const y = pp.getY().toBuffer();
+
+        const bt = Buffer.concat([x, y])
+
+        const kc = sha3.keccak256(bt)
+
+        return kc.slice(24)
+
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+
+(async () => {
+    let addressFound = false;
+    let privateKey, address;
+
+    privateKey = generatePrivateKey();
+    address = privateKeyToAddress(privateKey);
+
+    while (!addressFound) {
+        privateKey = generatePrivateKey();
+        address = privateKeyToAddress(privateKey);
+
+
+        if (address.startsWith(targetPrefix)) {
+            addressFound = true;
+        }
+    }
+
+    address = "0x" + address;
+
+    console.log("Private Key: " + privateKey);
+    console.log("Address: " + address);
 
 })();
+
+
